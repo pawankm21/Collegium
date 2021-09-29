b61e7416e17f3abd8ebd49642cd11f7038e44e1b
pawan <b419039@iiit-bh.ac.in>
2021-09-29 21:24:30 +0530

    removed styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React,{useState,useLayoutEffect} from 'react';
 function App() {
@@ -14,20 +14,6 @@ function App() {
   })
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <p>{ apiResponse}</p>
     </div>
   );
