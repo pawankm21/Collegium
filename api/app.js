b0a23eea8ec960663e0b0ef86e241862c6fb35ae
pawan <b419039@iiit-bh.ac.in>
2021-10-01 17:18:09 +0530

    refactored testAPI

--- api/app.js
+++ api/app.js
@@ -3,9 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var testAPIRouter = require("./routes/testAPI");
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var APIRouter = require("./routes/API");
+var indexRouter = require('./routes/APIdocs');
 var cors = require("cors");
 var app = express();
 
@@ -20,8 +19,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use("/testAPI", testAPIRouter);
+app.use("/API", APIRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import React,{useState,useLayoutEffect} from 'react';
 function App() {
   const [apiResponse, setApiResponse] = useState();
   function callAPI() {
-    fetch("http://localhost:9000/testAPI")
+    fetch("http://localhost:9000/API")
         .then(res => res.text())
         .then(res => setApiResponse( res ));
 }
