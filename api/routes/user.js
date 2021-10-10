be9c4391e24e118416ed339d022a06ee050e8d24
pawan <b419039@iiit-bh.ac.in>
2021-10-10 18:12:43 +0530

    createUser route

--- api/routes/user.js
+++ api/routes/user.js
@@ -5,13 +5,37 @@ const { User, Tag, Event } = require("../model");
 
 router.get("/getUser/:id", (req, res) => {
   console.log(req.params);
-  User.findOne({ id: req.params.id }, (err, user) => {
-    res.send({
-      id: user.id,
-      name: user.name,
-      email: user.email,
-      gender: user.gender,
-    });
+  User.findById(req.params.id, (err, user) => {
+    try {
+      res.send({
+        id: user._id,
+        name: user.name,
+        email: user.email,
+        gender: user.gender,
+      });
+    } catch {
+      res.send(err);
+    }
+  });
+});
+router.post("/createUser", (req, res) => {
+  const { name, email, gender, college } = req.body;
+  const newUser = new User({
+    name: name,
+    email: email,
+    gender: gender,
+    college: college,
+  });
+  newUser.save((err) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send({
+        statusCode: 200,
+        id: 1,
+        message: "User added succesfully!",
+      });
+    }
   });
 });
 module.exports = router;
