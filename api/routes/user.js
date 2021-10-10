17d71f2ce0e80a05245fe0787f305c2977183bbc
pawan <b419039@iiit-bh.ac.in>
2021-10-10 21:31:10 +0530

    updateUser endpoint

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,7 +2,6 @@ var express = require("express");
 var router = express.Router();
 const { User, Tag, Event } = require("../model");
 // all endpoints related to the user
-
 router.get("/getUser/:id", (req, res) => {
   console.log(req.params);
   User.findById(req.params.id, (err, user) => {
@@ -12,6 +11,10 @@ router.get("/getUser/:id", (req, res) => {
         name: user.name,
         email: user.email,
         gender: user.gender,
+        roll: user.roll,
+        college: user.college,
+        branch: user.branch,
+        dob: user.dob,
       });
     } catch {
       res.send(err);
@@ -19,23 +22,50 @@ router.get("/getUser/:id", (req, res) => {
   });
 });
 router.post("/createUser", (req, res) => {
-  const { name, email, gender, college } = req.body;
+  const { name, email, gender, college, branch, roll, dob } = req.body;
+  console.log(req.body);
   const newUser = new User({
     name: name,
     email: email,
     gender: gender,
     college: college,
+    branch: branch,
+    roll: roll,
+    dob: new Date(dob),
   });
   newUser.save((err) => {
     if (err) {
       res.send(err);
     } else {
       res.send({
-        statusCode: 200,
-        id: 1,
-        message: "User added succesfully!",
+        id: newUser._id,
+        message: "New User Created Successfully!",
       });
     }
   });
 });
+
+router.patch("/updateUser/:id", (req, res) => {
+  const { branch, roll, email } = req.body;
+  console.log(branch, roll, email);
+  User.updateOne(
+    { _id: req.params.id },
+    {
+      email: email,
+      branch: branch,
+      roll: roll,
+    },
+    (err) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send({
+          id: req.params.id,
+          message: "Updated Successfully!",
+        });
+      }
+    }
+  );
+});
+
 module.exports = router;
