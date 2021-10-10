d84de8a960748cb80d4ac26a7f2f8ce36d3d5e13
Abhinav Narang <abhinavn2904@gmail.com>
2021-10-10 12:33:02 +0530

    edited user profile form

--- client/src/components/UserProfileForm.jsx
+++ client/src/components/UserProfileForm.jsx
@@ -31,7 +31,7 @@ const UserProfileBig = () => {
 						className="h-48 w-48 rounded-full"
 					/>
 				</div>
-				<div className="bg-blue-400 mt-4 mb-10 py-2 px-2 rounded-full">
+				<div className="bg-blue-400 mt-4 mb-10 py-2 px-2 rounded-full font-mono text-sm">
 					<button className="">Change picture</button>
 				</div>
 			</div>
@@ -39,16 +39,16 @@ const UserProfileBig = () => {
 				<div className="ml-10 flex content-center">
 					<form action="" className="flex flex-col justify-center space-y-4" onSubmit={formSubmitHandler}>
 						<div className="">
-							<label htmlFor="name" className="">Name: </label>
-							<input type="text" name="name" id="name" className="bg-red-100 outline-none" ref={nameRef} />
+							<label htmlFor="name" className="font-mono text-sm">Name: </label>
+							<input type="text" name="name" id="name" className="bg-red-100 outline-none font-mono text-sm" ref={nameRef} />
 						</div>
 						<div className="">
-						    <label htmlFor="email">Email: </label>
-						    <input type="email" name="email" id="email" className="bg-red-100 outline-none" ref={emailRef} />
+						    <label className="font-mono text-sm" htmlFor="email">Email: </label>
+						    <input type="email" name="email" id="email" className="bg-red-100 outline-none font-mono text-center text-sm" ref={emailRef} />
 						</div>
 						<div className="">
-                            <label htmlFor="gender">Gender: </label>
-                            <select name="gender" id="gender" className="bg-red-100 outline-none" ref={genderRef}> 
+                            <label className="font-mono text-sm" htmlFor="gender">Gender: </label>
+                            <select name="gender" id="gender" className="bg-red-100 outline-none font-mono text-sm" ref={genderRef}> 
                                 <option value="select">Select</option>
                                 <option value="male">MALE</option>
                                 <option value="female">FEMALE</option>
@@ -58,18 +58,18 @@ const UserProfileBig = () => {
 						    {/* <input type="text" name="gender" id="gender" className="bg-red-100 outline-none" ref={genderRef} /> */}
 						</div>
 						<div className="">
-						    <label htmlFor="password">Password: </label>
-						    <input type="password" name="password" id="password" className="bg-red-100 outline-none" ref={passwordRef} />
+						    <label className="font-mono text-sm" htmlFor="password">Password: </label>
+						    <input type="password" name="password" id="password" className="bg-red-100 outline-none font-mono text-sm" ref={passwordRef} />
 						</div>
 						<div className="">
-						    <label htmlFor="college">College: </label>
-						    <input type="text" name="college" id="college" className="bg-red-100 outline-none" ref={collegeRef} />
+						    <label className="font-mono text-sm" htmlFor="college">College: </label>
+						    <input type="text" name="college" id="college" className="bg-red-100 outline-none font-mono text-sm" ref={collegeRef} />
 						</div>
 						<div className="">
-						    <label htmlFor="id">ID: </label>
-						    <input type="text" name="id" id="id" className="bg-red-100 outline-none" ref={idRef} />
+						    <label className="font-mono text-sm" htmlFor="id">ID: </label>
+						    <input type="text" name="id" id="id" className="bg-red-100 outline-none font-mono text-sm" ref={idRef} />
                         </div>
-                        <button className="bg-gray-400 mt-2 w-24 rounded-full">Submit</button>
+                        <button className="bg-green-400 mt-2 w-24 rounded-full font-mono text-sm">Submit</button>
 					</form>
 				</div>
 			</div>
