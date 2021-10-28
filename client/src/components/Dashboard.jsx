ea15300a5db9eef002cb923f5c2c9b0a0a8169ec
pawan <b419039@iiit-bh.ac.in>
2021-10-28 19:10:14 +0530

    dashboard styling

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -1,17 +1,32 @@
-import React from 'react'
-import EventCard from './EventCard'
-import EventCardSmall from './EventCardSmall'
-import Navbar from './Navbar';
+import React from "react";
+import EventCard from "./EventCard";
+import EventCardSmall from "./EventCardSmall";
+import Navbar from "./Navbar";
+import UserProfileSmall from "./UserProfileSmall";
 export default function Dashboard() {
-    return (
-      <>
-        <Navbar />
-        {/* <EventCard /> */}
-            <div className="grid grid-cols-3">
-                
-          <EventCardSmall  />
-       
+  const events = () => {
+    var ret = [];
+    for (let i = 0; i < 5; i++) {
+      ret.push(
+        <div className=" pt-2 pb-1">
+          <EventCardSmall />
         </div>
-      </>
-    );
+      );
+      }
+      return ret;
+  };
+  return (
+    <>
+      <Navbar />
+      <div className=" grid-col"></div>
+      <div className="w-1/3 ml-5">
+        <div className="mb-5 mt-5">
+          <UserProfileSmall />
+        </div>
+        <section className=" grid grid-col-1 justify-center rounded-lg bg-red-500">
+          {events()}
+        </section>
+      </div>
+    </>
+  );
 }
--- client/src/components/UserProfileSmall.jsx
+++ client/src/components/UserProfileSmall.jsx
@@ -1,31 +1,33 @@
 const UserProfileSmall = () => {
-    const username = "John Doe";
-    const upcomingEvents = 13;
-    const tags = ['c++', 'python', 'java','sql','html','css'];
+  const username = "John Doe";
+  const upcomingEvents = 13;
+  const tags = ["c++", "python", "java", "sql", "html", "css"];
 
-	return (
-		<div className="h-auto py-10 w-auto rounded-lg bg-blue-100 max-w-lg">
-			<div className="mt-3">
-				<div className="inline-block ml-10  mr-2">
-					<img
-						src="/images/avtar.png"
-						alt="Avatar"
-						class="h-24 w-24 rounded-full inline"
-					/>
-				</div>
-                <div className="inline-block ml-2">
-                    <p className="text-xl">{username}</p>
-                    <p className="text-xl">{upcomingEvents} Upcoming events</p>
-                </div>
+  return (
+    <div className="w-auto py-10  rounded-lg bg-blue-100 h-auto">
+      <div className="mt-2">
+        <div className="inline-block ml-2 mr-2">
+          <img
+            src="/images/avtar.png"
+            alt="Avatar"
+            class="h-16 w-16 rounded-full inline"
+          />
+        </div>
+        <div className="inline-block ml-2 ">
+          <p className="text-lg">{username}</p>
+          <p className="text-lg">{upcomingEvents} Upcoming events</p>
+        </div>
+      </div>
+      <div className="ml-2 flex mt-4 justify-center">
+        <ul className="">
+          {tags.map((tag) => (
+            <div className=" inline-block m-1 bg-red-300 rounded-lg w-auto text-xs px-2 py-1">
+              <li className="">{tag}</li>
             </div>
-            <div className="ml-32 inline-block">
-                <ul className="space-x-4">
-                    {tags.map(tag => (
-                        <div className="inline-block m-1 bg-red-300 rounded-lg w-auto px-2 py-1"><li className="">{tag}</li></div>
-                    ))}
-                </ul>
-            </div>
-		</div>
-	);
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 };
 export default UserProfileSmall;
