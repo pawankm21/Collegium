a2d6c3eb93ef6f329ca804e42a0d28941fda5f78
pawan <b419039@iiit-bh.ac.in>
2021-11-30 23:54:18 +0530

    add tab

--- client/src/components/Dashboard.jsx
+++ client/src/components/Dashboard.jsx
@@ -3,6 +3,7 @@ import EventCard from "./EventCard";
 import EventCardSmall from "./EventCardSmall";
 import Navbar from "./Navbar";
 import UserProfileSmall from "./UserProfileSmall";
+import { Tab } from "@headlessui/react";
 export default function Dashboard() {
   const events = () => {
     var ret = [];
@@ -14,6 +15,112 @@ export default function Dashboard() {
       );
     }
     return ret;
+  };
+  const EVENT = {
+    Upcoming: [
+      <div className="m-1">
+        <EventCard
+          imageUrl="https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg"
+          name="hello world"
+          message=" lorem ipsum dolores"
+          tags={["lorem", "ipsum", "dolores"]}
+        />
+      </div>,
+      <div className="m-1">
+        <EventCard
+          imageUrl="https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg"
+          name="hello world"
+          message=" lorem ipsum dolores"
+          tags={["lorem", "ipsum", "dolores"]}
+        />
+      </div>,
+      <div className="m-1">
+        <EventCard
+          imageUrl="https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg"
+          name="hello world"
+          message=" lorem ipsum dolores"
+          tags={["lorem", "ipsum", "dolores"]}
+        />
+      </div>,
+    ],
+    Past: [
+      <div className="m-1">
+        <EventCard
+          imageUrl="https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg"
+          name="hello world"
+          message=" lorem ipsum dolores"
+          tags={["lorem", "ipsum", "dolores"]}
+        />
+      </div>,
+      <div className="m-1">
+        <EventCard
+          imageUrl="https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg"
+          name="hello world"
+          message=" lorem ipsum dolores"
+          tags={["lorem", "ipsum", "dolores"]}
+        />
+      </div>,
+      <div className="m-1">
+        <EventCard
+          imageUrl="https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg"
+          name="hello world"
+          message=" lorem ipsum dolores"
+          tags={["lorem", "ipsum", "dolores"]}
+        />
+      </div>,
+    ],
+    Ongoing: [
+      <div className="m-1">
+        <EventCard
+          imageUrl="https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg"
+          name="hello world"
+          message=" lorem ipsum dolores"
+          tags={["lorem", "ipsum", "dolores"]}
+        />
+      </div>,
+      <div className="m-1">
+        <EventCard
+          imageUrl="https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg"
+          name="hello world"
+          message=" lorem ipsum dolores"
+          tags={["lorem", "ipsum", "dolores"]}
+        />
+      </div>,
+      <div className="m-1">
+        <EventCard
+          imageUrl="https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg"
+          name="hello world"
+          message=" lorem ipsum dolores"
+          tags={["lorem", "ipsum", "dolores"]}
+        />
+      </div>,
+    ],
+    "Your Events": [
+      <div className="m-1">
+        <EventCard
+          imageUrl="https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg"
+          name="hello world"
+          message=" lorem ipsum dolores"
+          tags={["lorem", "ipsum", "dolores"]}
+        />
+      </div>,
+      <div className="m-1">
+        <EventCard
+          imageUrl="https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg"
+          name="hello world"
+          message=" lorem ipsum dolores"
+          tags={["lorem", "ipsum", "dolores"]}
+        />
+      </div>,
+      <div className="m-1">
+        <EventCard
+          imageUrl="https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg"
+          name="hello world"
+          message=" lorem ipsum dolores"
+          tags={["lorem", "ipsum", "dolores"]}
+        />
+      </div>,
+    ],
   };
 
   return (
@@ -27,57 +134,44 @@ export default function Dashboard() {
             <h1 className="text-2xl font-bold  bg-white w-full sticky top-0 pl-4">
               Registrations
             </h1>
-            <div className="pr-4 border-4  divide-solid">
-              {events()}
+            <div className="pr-4 border-4  divide-solid">{events()}</div>
+          </div>
+        </div>
+        <Tab.Group>
+          <div className="pl-4 pb-4 pr-4 shadow-2xl h-screen overflow-y-auto rounded-xl ">
+            <div className="grid grid-cols-4 gap-1">
+              <Tab.List>
+                {" "}
+                <span className="text-2xl font-bold  bg-white w-full sticky top-0 ">
+                  Upcoming
+                </span>
+              </Tab.List>
+              <Tab.List>
+                {" "}
+                <span className="text-2xl font-bold  bg-white w-full sticky top-0 ">
+                  Ongoing
+                </span>
+              </Tab.List>
+              <Tab.List>
+                {" "}
+                <span className="text-2xl font-bold  bg-white w-full sticky top-0 ">
+                  Past Events
+                </span>
+              </Tab.List>{" "}
+              <Tab.List>
+                {" "}
+                <span className="text-2xl font-bold  bg-white w-full sticky top-0 ">
+                  Your Events
+                </span>
+              </Tab.List>
             </div>
+             <Tab.Panels>
+        <Tab.Panel>Content 1</Tab.Panel>
+        <Tab.Panel>Content 2</Tab.Panel>
+        <Tab.Panel>Content 3</Tab.Panel>
+      </Tab.Panels>
           </div>
-        </div>
-
-        <div className="pl-4 pb-4 pr-4 shadow-2xl h-screen overflow-y-auto rounded-xl ">
-          <h1 className="text-2xl font-bold  bg-white w-full sticky top-0 ">
-            Upcoming Events
-          </h1>
-          <div className="mt-2 m-1 mb-2   ">
-            <EventCard
-              imageUrl="https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg"
-              name="hello world"
-              message=" lorem ipsum dolores"
-              tags={["lorem", "ipsum", "dolores"]}
-            />
-          </div>{" "}
-          <div className="m-1">
-            <EventCard
-              imageUrl="https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg"
-              name="hello world"
-              message=" lorem ipsum dolores"
-              tags={["lorem", "ipsum", "dolores"]}
-            />
-          </div>{" "}
-          <div className="m-1">
-            <EventCard
-              imageUrl="https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg"
-              name="hello world"
-              message=" lorem ipsum dolores"
-              tags={["lorem", "ipsum", "dolores"]}
-            />
-          </div>{" "}
-          <div className="m-1">
-            <EventCard
-              imageUrl="https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg"
-              name="hello world"
-              message=" lorem ipsum dolores"
-              tags={["lorem", "ipsum", "dolores"]}
-            />
-          </div>{" "}
-          <div className="m-1">
-            <EventCard
-              imageUrl="https://www.pixsy.com/wp-content/uploads/2021/04/ben-sweet-2LowviVHZ-E-unsplash-1.jpeg"
-              name="hello world"
-              message=" lorem ipsum dolores"
-              tags={["lorem", "ipsum", "dolores"]}
-            />
-          </div>
-        </div>
+        </Tab.Group>
       </div>
       <footer className="h-16 "></footer>
     </>
