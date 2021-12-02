bbe9282f82e7cc6cfc69c554230fe264e1ae60b6
pawan <b419039@iiit-bh.ac.in>
2021-12-02 22:45:56 +0530

    show event styled

--- client/src/components/EventCard.jsx
+++ client/src/components/EventCard.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import Tags from './Tags'
 import { Link } from 'react-router-dom'
-export default function EventCard({name, message, tags, imageUrl} )  {
+export default function EventCard({ name, message, tags, imageUrl, }) {
+    var id = 1;
     return (
         <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
             <div class="md:flex">
@@ -10,7 +11,7 @@ export default function EventCard({name, message, tags, imageUrl} )  {
                 </div>
                 <div class="p-8">
                 <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Ends in 3 days</div>
-                <Link to="#"  class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">{ name}</Link>
+                <Link to={`events/${id}`}  class="block mt-1 text-lg leading-tight font-medium text-black hover:underline">{ name}</Link>
                 <p class="mt-2 text-gray-500">{ message}</p>
                 <Tags tags = { tags}/>
                 </div>
